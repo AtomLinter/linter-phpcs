4d50e40c6ec306b708ec954da9ce29927d2d891c
Landon Abney <Landonabney@gmail.com>
2016-08-26 13:23:33 -0700

    Cleanup the specs a bit

--- spec/linter-phpcs-spec.js
+++ spec/linter-phpcs-spec.js
@@ -45,17 +45,13 @@ describe('The phpcs provider for Linter', () => {
     it('verifies the first message', () => {
       waitsForPromise(() =>
         lint(editor).then(messages => {
-          expect(messages[0].type).toBeDefined();
           expect(messages[0].type).toBe('ERROR');
-          expect(messages[0].html).toBeDefined();
+          expect(messages[0].text).not.toBeDefined();
           expect(messages[0].html).toBe('' +
             '<span class="badge badge-flexible">Generic.PHP.LowerCaseConstant.Found</span>' +
             ' TRUE, FALSE and NULL must be lowercase; ' +
             'expected &quot;true&quot; but found &quot;TRUE&quot;');
-          expect(messages[0].filePath).toBeDefined();
-          expect(messages[0].filePath).toMatch(/.+bad\.php$/);
-          expect(messages[0].range).toBeDefined();
-          expect(messages[0].range.length).toBe(2);
+          expect(messages[0].filePath).toBe(badPath);
           expect(messages[0].range).toEqual([[1, 5], [1, 6]]);
         })
       );
